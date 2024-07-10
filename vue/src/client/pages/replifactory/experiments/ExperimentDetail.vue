ef25bc5a5ee7133b03a9fb79d485dffaccab358b
Fedor Gagarin <fddgagarin@gmail.com>
2024-07-10 22:41:36 +0900

    add button back to experiment detail page

--- vue/src/client/pages/replifactory/experiments/ExperimentDetail.vue
+++ vue/src/client/pages/replifactory/experiments/ExperimentDetail.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { shallowRef, markRaw, onBeforeMount, defineAsyncComponent } from "vue";
 import { useStore } from "vuex";
-import { useRoute } from "vue-router";
+import { useRouter, useRoute } from "vue-router";
 import { componentLoader } from "@/plugins.js";
+import { CIcon } from "@coreui/icons-vue";
 
 const store = useStore();
 const route = useRoute();
+const router = useRouter();
 
 const dashboardComponent = shallowRef(markRaw({
     template: "<div>Loading dashboard...</div>",
@@ -27,10 +29,16 @@ onBeforeMount(async () => {
         console.error(error);
     });
 });
+
+const goBack = () => {
+    router.back();
+};
 </script>
 
 <template>
-    <div class="container">
+    <div class="container my-2">
+        <button class="btn btn-secondary" @click="goBack"><CIcon icon="cilArrowLeft" /> Go Back</button>
+
         <component :is="dashboardComponent" />
     </div>
 </template>
